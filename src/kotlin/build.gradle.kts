b540e6cee30edb1a5fde0d0f71f6aa484184a4b2
Cy Scott <cy.a.scott@live.com>
2026-05-25 16:30:52 -0400

    fix: cicd

--- src/kotlin/build.gradle.kts
+++ src/kotlin/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.1.10"
     jacoco
     `maven-publish`
+    signing
 }
 
 group = "com.cyascott"
@@ -45,6 +46,11 @@ kotlin {
     jvmToolchain(17)
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
@@ -52,6 +58,32 @@ publishing {
             groupId = project.group.toString()
             artifactId = "poly-bus"
             version = project.version.toString()
+
+            pom {
+                name.set("PolyBus")
+                description.set("A polyglot messaging framework for building interoperable applications across multiple programming languages.")
+                url.set("https://github.com/CyAScott/poly-bus")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/mit")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("cyascott")
+                        name.set("Cy Scott")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/CyAScott/poly-bus.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/CyAScott/poly-bus.git")
+                    url.set("https://github.com/CyAScott/poly-bus")
+                }
+            }
         }
     }
 
@@ -72,5 +104,23 @@ publishing {
                     ?: System.getenv("MAVEN_PASSWORD")
             }
         }
+    }
+}
+
+signing {
+    val signingKeyId = (findProperty("signingInMemoryKeyId") as String?)
+        ?: System.getenv("SIGNING_KEY_ID")
+    val signingKey = (findProperty("signingInMemoryKey") as String?)
+        ?: System.getenv("SIGNING_KEY")
+    val signingPassword = (findProperty("signingInMemoryKeyPassword") as String?)
+        ?: System.getenv("SIGNING_PASSWORD")
+
+    if (!signingKey.isNullOrBlank()) {
+        if (!signingKeyId.isNullOrBlank()) {
+            useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+        } else {
+            useInMemoryPgpKeys(signingKey, signingPassword)
+        }
+        sign(publishing.publications)
     }
 }
